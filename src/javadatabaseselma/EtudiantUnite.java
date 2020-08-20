b08c943266bc65850732b16ce6695afde5f47003
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-19 18:23:29 -0700

    Refactoring : EtudiantUnite class

--- src/javadatabaseselma/EtudiantUnite.java
+++ src/javadatabaseselma/EtudiantUnite.java
@@ -33,5 +33,75 @@ public class EtudiantUnite {
         this.NOTE_TP = new SimpleIntegerProperty(NOTE_TP);
         this.NOTE_EXAMEN = new SimpleIntegerProperty(NOTE_EXAMEN);
     }
+
+    /**
+     * @return the MATRICULE_ETU
+     */
+    public SimpleIntegerProperty getMATRICULE_ETU() {
+        return MATRICULE_ETU;
+    }
+
+    /**
+     * @param MATRICULE_ETU the MATRICULE_ETU to set
+     */
+    public void setMATRICULE_ETU(SimpleIntegerProperty MATRICULE_ETU) {
+        this.MATRICULE_ETU = MATRICULE_ETU;
+    }
+
+    /**
+     * @return the CODE_UNITE
+     */
+    public SimpleStringProperty getCODE_UNITE() {
+        return CODE_UNITE;
+    }
+
+    /**
+     * @param CODE_UNITE the CODE_UNITE to set
+     */
+    public void setCODE_UNITE(SimpleStringProperty CODE_UNITE) {
+        this.CODE_UNITE = CODE_UNITE;
+    }
+
+    /**
+     * @return the NOTE_CC
+     */
+    public SimpleIntegerProperty getNOTE_CC() {
+        return NOTE_CC;
+    }
+
+    /**
+     * @param NOTE_CC the NOTE_CC to set
+     */
+    public void setNOTE_CC(SimpleIntegerProperty NOTE_CC) {
+        this.NOTE_CC = NOTE_CC;
+    }
+
+    /**
+     * @return the NOTE_TP
+     */
+    public SimpleIntegerProperty getNOTE_TP() {
+        return NOTE_TP;
+    }
+
+    /**
+     * @param NOTE_TP the NOTE_TP to set
+     */
+    public void setNOTE_TP(SimpleIntegerProperty NOTE_TP) {
+        this.NOTE_TP = NOTE_TP;
+    }
+
+    /**
+     * @return the NOTE_EXAMEN
+     */
+    public SimpleIntegerProperty getNOTE_EXAMEN() {
+        return NOTE_EXAMEN;
+    }
+
+    /**
+     * @param NOTE_EXAMEN the NOTE_EXAMEN to set
+     */
+    public void setNOTE_EXAMEN(SimpleIntegerProperty NOTE_EXAMEN) {
+        this.NOTE_EXAMEN = NOTE_EXAMEN;
+    }
     
 }
