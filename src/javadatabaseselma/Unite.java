995ccce9fff24a2baa77767dc5da5020c0246925
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-19 18:30:02 -0700

    add Unite class

---
+++ src/javadatabaseselma/Unite.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadatabaseselma;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author b
+ */
+public class Unite {
+    
+    private SimpleIntegerProperty MATRICULE_ETU;
+    private SimpleStringProperty CODE_UNITE;
+    private SimpleIntegerProperty NOTE_CC;
+    private SimpleIntegerProperty NOTE_TP;
+}
