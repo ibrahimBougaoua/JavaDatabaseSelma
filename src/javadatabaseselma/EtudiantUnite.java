7f9a554e05049a4266768c73dbc3b687d41a78d8
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-19 18:18:57 -0700

    add EtudiantUnite class

---
+++ src/javadatabaseselma/EtudiantUnite.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javadatabaseselma;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ *
+ * @author b
+ */
+public class EtudiantUnite {
+    
+    private SimpleIntegerProperty MATRICULE_ETU;
+    private SimpleStringProperty  NOM_ETU;
+    private SimpleStringProperty  PRENOM_ETU;
+    private SimpleStringProperty  DATE_NAISSANCE;
+    private SimpleStringProperty  ADDRESSE;
+    
+}
