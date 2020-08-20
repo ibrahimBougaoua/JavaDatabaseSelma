601d768e0105c831bf3e7977ed6d7f166a784b39
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-20 11:05:06 -0700

    Refactoring : EtudiantController.java

--- src/javadatabaseselma/EtudiantController.java
+++ src/javadatabaseselma/EtudiantController.java
@@ -6,6 +6,7 @@
 package javadatabaseselma;
 
 import com.mysql.jdbc.PreparedStatement;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -13,9 +14,16 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.SceneAntialiasing;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -42,18 +50,68 @@ public class EtudiantController implements Initializable {
     Label getDn;
     
     @FXML
+    Label errorValider;
+    
+    @FXML
+    Button resetValiderEt;
+    
+    @FXML
+    Button backToLoginPage;
+    
+    @FXML
+    public void ResetValiderForm(ActionEvent e) throws SQLException {
+    getMatriculeField.setText("");
+    }
+    
+    @FXML
     public void validerDataByMatricule(ActionEvent e) throws SQLException {
+        
+        boolean isNumeric = getMatriculeField.getText().chars().allMatch( Character::isDigit );
+        errorValider.setText("");
+
+        if(isNumeric)
+        {
+        
         String sql = "SELECT * FROM etudiant where MATRICULE_ETU = " + Integer.parseInt(getMatriculeField.getText());
         Connection connection = Database.getConnectionDb();
         PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(sql);
         ResultSet rs = preparedStatement.executeQuery(sql );
         
+        getMatricule.setText("");
+        getNom.setText(""); 
+        getPrenom.setText("");
+        getDn.setText("");
+           
         while (rs.next()) {
            getMatricule.setText(rs.getString("MATRICULE_ETU").toString());
            getNom.setText(rs.getString("NOM_ETU").toString()); 
            getPrenom.setText(rs.getString("PRENOM_ETU").toString());
            getDn.setText(rs.getString("DATE_NAISSANCE").toString());
         }
+        } else {
+            errorValider.setText("Votre Matricule est faux ou n'existe pas");
+            errorValider.setTextFill(Color.rgb(210, 39, 30));
+        }
+        
+        if(getMatriculeField.getText().equals(""))
+        {
+            errorValider.setText("Votre Matricule est faux ou n'existe pas");
+            errorValider.setTextFill(Color.rgb(210, 39, 30));
+        }
+    }
+    
+    @FXML
+    public void backToLoginPage(ActionEvent event) throws IOException  {
+        Stage stage = (Stage) backToLoginPage.getScene().getWindow();
+        stage.close();
+        Stage login = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+        //Scene scene = new Scene(root, 450, 300);
+        Scene scene = new Scene(root, -1, -1, true, SceneAntialiasing.BALANCED);
+        scene.getStylesheets().add(getClass().getResource("login.css").toExternalForm());
+        login.setTitle("Login");
+        login.setScene(scene);
+        login.show();
     }
     
     /**
