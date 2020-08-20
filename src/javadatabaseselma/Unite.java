aa3ff24f6962b6d27e99986f367f993917b55858
ibrahimBougaoua <ibrahim.bougaoua@etu.usthb.dz>
2020-08-19 18:33:06 -0700

    Refactoring : Unite class

--- src/javadatabaseselma/Unite.java
+++ src/javadatabaseselma/Unite.java
@@ -14,8 +14,22 @@ import javafx.beans.property.SimpleStringProperty;
  */
 public class Unite {
     
-    private SimpleIntegerProperty MATRICULE_ETU;
-    private SimpleStringProperty CODE_UNITE;
-    private SimpleIntegerProperty NOTE_CC;
-    private SimpleIntegerProperty NOTE_TP;
+    private SimpleStringProperty CODE_UNITE ;
+    private SimpleStringProperty LIBELLE;
+    private SimpleIntegerProperty NBR_HEURES;
+    private SimpleIntegerProperty MATRICULE_ENS ;
+    
+    public Unite()
+    {
+        
+    }
+    
+    public Unite(String CODE_UNITE ,String LIBELLE,int NBR_HEURES,int MATRICULE_ENS)
+    {
+        this.CODE_UNITE = new SimpleStringProperty(CODE_UNITE);
+        this.LIBELLE = new SimpleStringProperty(LIBELLE);
+        this.NBR_HEURES = new SimpleIntegerProperty(NBR_HEURES);
+        this.MATRICULE_ENS  = new SimpleIntegerProperty(MATRICULE_ENS );
+    }
+    
 }
